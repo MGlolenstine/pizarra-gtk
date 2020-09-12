d3f0a8ade7a6891721449f725b41bc741b298de9
Abraham Toriz <categulario@gmail.com>
2020-09-12 00:28:14 -0500

    this deploy script will make everything easier

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env /bin/bash
+
+if [[ -f .env ]]; then
+    source .env
+fi
+
+rm -rf build
+mkdir -p build
+
+cp target/release/pizarra build/pizarra
+cp res/pizarra.svg build/pizarra.svg
+cp res/pizarra.desktop build/pizarra.desktop
+cp CHANGELOG.md build/CHANGELOG.md
+
+version=$(git describe --long | sed 's/\([^-]*-\)g/r\1/;s/-/./g;s/v//g')
+arch=x86_64
+archivename=pizarra-$version-$arch.tar.gz
+
+tar -cvzf $archivename build/
+scp $archivename $DEPLOY_USER@$DEPLOY_HOST:$RELEASES_PATH/$archivename
+rm $archivename
