6ee89f7d7867509187ab331aefe783d05d889975
Abraham Toriz <categulario@gmail.com>
2021-01-11 20:09:43 -0600

    extend deploy script with notification on channel

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,19 @@ arch=x86_64
 archivename=pizarra-$version-$arch.tar.gz
 
 tar -cvzf $archivename build/
-scp $archivename $DEPLOY_USER@$DEPLOY_HOST:$RELEASES_PATH/$archivename
-rm $archivename
+# scp $archivename $DEPLOY_USER@$DEPLOY_HOST:$RELEASES_PATH/$archivename
+# rm $archivename
+
+change=`git tag --format "%(refname:strip=2) %(contents:subject)" --sort version:refname | tail -n1`
+
+cat << EOF | curl \
+    -X POST \
+    -H "Content-Type: application/json" \
+    "https://api.telegram.org/bot$BOT_KEY/sendMessage" \
+    -d @-
+{
+    "chat_id": $CHANNEL_ID,
+    "parse_mode": "Markdown",
+    "text": "✒️ Pizarra **$version** está disponible para descargar:\n\nhttps://pizarra.categulario.tk/releases/\n\nÚltimo cambio: __${change}__"
+}
+EOF
