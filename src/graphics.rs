246f8531a31dfd555ac762c7a789ea529c3feada
Abraham Toriz <categulario@gmail.com>
2021-01-20 18:39:58 -0600

    use pizarra 0.9

--- src/graphics.rs
+++ src/graphics.rs
@@ -14,38 +14,15 @@ pub trait Drawable {
 impl Drawable for DrawCommand {
     fn draw(self, ctx: &Context, t: Transform) {
         match self {
-            DrawCommand::Line {
-                color, line, thickness,
-            } => {
-                ctx.set_line_width(thickness * t.scale_factor());
-                ctx.set_source_rgb(color.r, color.g, color.b);
-                ctx.set_line_cap(LineCap::Round);
-                ctx.set_line_join(LineJoin::Round);
-
-                let mut iter_points = line.iter();
-
-                if let Some(first_point) = iter_points.next() {
-                    let p = t.to_screen_coordinates(*first_point);
-
-                    ctx.move_to(p.x, p.y);
-
-                    for point in iter_points {
-                        let p = t.to_screen_coordinates(*point);
-                        ctx.line_to(p.x, p.y);
-                    }
-
-                    ctx.stroke();
-                }
-            },
             DrawCommand::Path {
-                color, line, thickness,
+                color, commands, thickness,
             } => {
                 ctx.set_line_width(thickness * t.scale_factor());
                 ctx.set_source_rgb(color.r, color.g, color.b);
                 ctx.set_line_cap(LineCap::Round);
                 ctx.set_line_join(LineJoin::Round);
 
-                for point in line.iter() {
+                for point in commands.iter() {
                     if point.is_nan() {
                         dbg!(point);
                     }
@@ -54,6 +31,10 @@ impl Drawable for DrawCommand {
                         PathCommand::MoveTo(p) => {
                             let p = t.to_screen_coordinates(*p);
                             ctx.move_to(p.x, p.y);
+                        },
+                        PathCommand::LineTo(p) => {
+                            let p = t.to_screen_coordinates(*p);
+                            ctx.line_to(p.x, p.y);
                         },
                         PathCommand::CurveTo(c) => {
                             let pt1 = t.to_screen_coordinates(c.pt1);
