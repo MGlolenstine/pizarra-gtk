427b09dddb644b589be5084f467b9bfb03d069b1
Abraham Toriz <categulario@gmail.com>
2021-11-01 19:37:48 -0600

    is this the end?

---
+++ scripts/release-aur-bin.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# some useful variables
+VERSION=${CI_COMMIT_TAG:1}
+ARCHIVENAME=pizarra_${CI_COMMIT_TAG:1}_x86-64.tar.gz
+
+# clone the repo
+git clone $BIN_REPO_URL pizarra-bin
+
+# enter it
+cd pizarra-bin
+
+# get the sum from the artifacts
+SUM=( `cat $ARCHIVENAME.sum` )
+
+# Generate the PKGBUILD
+echo "# Maintainer: Abraham Toriz <categulario at gmail dot com>
+pkgname=pizarra-bin
+pkgver=$VERSION
+pkgrel=1
+pkgdesc='A free-hand vector drawing application with infinite canvas'
+arch=('x86_64')
+url='https://gitlab.com/categulario/pizarra-gtk'
+license=('GPL3')
+depends=('gtk3')
+provides=('pizarra')
+conflicts=('pizarra')
+source=(\"https://pizarra.categulario.tk/releases/pizarra-\$pkgver-\$arch.tar.gz\")
+sha256sums=('$SUM')
+
+package() {
+    cd \"\$srcdir/build\"
+    install -Dm755 pizarra \"\$pkgdir\"/usr/bin/pizarra
+    install -Dm644 pizarra.desktop \"\$pkgdir\"/usr/share/applications/pizarra.desktop
+    install -Dm644 pizarra.svg \"\$pkgdir\"/usr/share/icons/hicolor/scalable/apps/pizarra.svg
+
+    install -Dm644 README.md \"\$pkgdir\"/usr/share/doc/pizarra/README.md
+    install -Dm644 LICENSE \"\$pkgdir\"/usr/share/doc/pizarra/LICENSE
+    install -Dm644 CHANGELOG.md \"\$pkgdir\"/usr/share/doc/pizarra/changelog
+}
+" | tee PKGBUILD > /dev/null
+
+makepkg --printsrcinfo > .SRCINFO
+git add .
+git commit -m "Release version $VERSION"
+git push
---
+++ scripts/release-aur-git.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Needed variables
+VERSION=${CI_COMMIT_TAG:1}
+
+# clone the repo
+git clone $GIT_REPO_URL pizarra-git
+
+# enter it
+cd pizarra-git
+
+echo "# Maintainer: Abraham Toriz <categulario at gmail dot com>
+pkgname=pizarra-git
+pkgver=$VERSION
+pkgrel=1
+pkgdesc='Simple Gtk drawing application'
+arch=('i686' 'x86_64')
+url='https://gitlab.com/categulario/pizarra-gtk'
+license=('GPL3')
+depends=('gtk3')
+makedepends=('cargo' 'git')
+provides=('pizarra')
+conflicts=('pizarra')
+source=('pizarra-git::git+https://gitlab.com/categulario/pizarra-gtk')
+sha256sums=('SKIP')
+
+pkgver() {
+    cd \"\$pkgname\"
+    printf \"%s\" \"\$(git describe --long | sed 's/\([^-]*-\)g/r\1/;s/-/./g;s/v//g')\"
+}
+
+build() {
+    cd \"\$pkgname\"
+    cargo build --release --locked
+}
+
+package() {
+    cd \"\$pkgname\"
+    install -Dm755 target/release/pizarra \"\$pkgdir\"/usr/bin/pizarra
+    install -Dm644 res/pizarra.desktop \"\$pkgdir\"/usr/share/applications/pizarra.desktop
+    install -Dm644 res/icons/tk.categulario.pizarra.svg \"\$pkgdir\"/usr/share/icons/hicolor/scalable/apps/pizarra.svg
+
+    install -Dm644 README.md \"\$pkgdir\"/usr/share/doc/pizarra/README.md
+    install -Dm644 LICENSE \"\$pkgdir\"/usr/share/doc/pizarra/LICENSE
+    install -Dm644 CHANGELOG.md \"\$pkgdir\"/usr/share/doc/pizarra/changelog
+}" | tee PKGBUILD > /dev/null
+
+makepkg --printsrcinfo > .SRCINFO
+git add .
+git commit -m "Release version $VERSION"
+git push
