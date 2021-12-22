6eadf5d5b315d0db56df8e1e959dce2b473777e7
Abraham Toriz <categulario@gmail.com>
2021-12-21 23:02:12 -0600

    handle shapes that draw in screen units

--- src/graphics.rs
+++ src/graphics.rs
@@ -4,52 +4,68 @@ use cairo::{Context, LineCap, LineJoin, Matrix};
 
 use pizarra::{
     draw_commands::DrawCommand, transform::Transform,
-    shape::path::{PathCommand, CubicBezierCurve},
+    path_command::{PathCommand, CubicBezierCurve},
+    point::Point, color::Color,
 };
 
+fn draw_path<T: Point>(ctx: &Context, color: Color, commands: &[PathCommand<T>], thickness: f64) {
+    ctx.set_line_width(thickness);
+    ctx.set_source_rgba(color.float_r(), color.float_g(), color.float_b(), color.float_alpha());
+    ctx.set_line_cap(LineCap::Round);
+    ctx.set_line_join(LineJoin::Round);
+
+    for point in commands.iter() {
+        match *point {
+            PathCommand::MoveTo(p) => {
+                ctx.move_to(p.x(), p.y());
+            },
+            PathCommand::LineTo(p) => {
+                ctx.line_to(p.x(), p.y());
+            },
+            PathCommand::CurveTo(CubicBezierCurve { pt1, pt2, to }) => {
+                ctx.curve_to(pt1.x(), pt1.y(), pt2.x(), pt2.y(), to.x(), to.y());
+            },
+        }
+    }
+
+    ctx.stroke().unwrap();
+}
+
+fn draw_circle<T: Point>(ctx: &Context, thickness: f64, center: T, radius: f64, color: Color) {
+    ctx.set_source_rgba(color.float_r(), color.float_g(), color.float_b(), color.float_alpha());
+    ctx.arc(center.x(), center.y(), radius, 0.0, 2.0*PI);
+    ctx.set_line_width(thickness);
+    ctx.stroke().unwrap();
+}
+
 pub trait Drawable {
-    fn draw(self, ctx: &Context, t: Transform);
+    fn draw(&self, ctx: &Context, t: Transform);
 }
 
 impl Drawable for DrawCommand {
-    fn draw(self, ctx: &Context, t: Transform) {
-        ctx.save().unwrap();
-        ctx.transform(Matrix::new(t.xx, t.yx, t.xy, t.yy, t.x0, t.y0));
+    fn draw(&self, ctx: &Context, t: Transform) {
+        match self {
+            DrawCommand::Path { .. } | DrawCommand::Circle { .. } | DrawCommand::Ellipse { .. } => {
+                ctx.save().unwrap();
+                ctx.transform(Matrix::new(t.xx, t.yx, t.xy, t.yy, t.x0, t.y0));
+            }
+            _ => {}
+        }
 
         match self {
             DrawCommand::Path {
                 color, commands, thickness,
-            } => {
-                ctx.set_line_width(thickness);
-                ctx.set_source_rgba(color.float_r(), color.float_g(), color.float_b(), color.float_alpha());
-                ctx.set_line_cap(LineCap::Round);
-                ctx.set_line_join(LineJoin::Round);
+            }  => {
+                draw_path(ctx, *color, commands, *thickness);
+            }
 
-                for point in commands.iter() {
-                    match *point {
-                        PathCommand::MoveTo(p) => {
-                            ctx.move_to(p.x, p.y);
-                        },
-                        PathCommand::LineTo(p) => {
-                            ctx.line_to(p.x, p.y);
-                        },
-                        PathCommand::CurveTo(CubicBezierCurve { pt1, pt2, to }) => {
-                            ctx.curve_to(pt1.x, pt1.y, pt2.x, pt2.y, to.x, to.y);
-                        },
-                    }
-                }
-
-                ctx.stroke().unwrap();
-            },
-            DrawCommand::Circle {
+            &DrawCommand::Circle {
                 thickness, center, radius, color,
             } => {
-                ctx.set_source_rgba(color.float_r(), color.float_g(), color.float_b(), color.float_alpha());
-                ctx.arc(center.x, center.y, radius, 0.0, 2.0*PI);
-                ctx.set_line_width(thickness);
-                ctx.stroke().unwrap();
-            },
-            DrawCommand::Ellipse {
+                draw_circle(ctx, thickness, center, radius, color);
+            }
+
+            &DrawCommand::Ellipse {
                 thickness, color, center, semimajor, semiminor, angle,
             } => {
                 if semimajor == 0.0 || semiminor == 0.0 {
@@ -66,9 +82,26 @@ impl Drawable for DrawCommand {
                 ctx.arc(0., 0., 1., 0., 2.0 * PI);
                 ctx.restore().unwrap();
                 ctx.stroke().unwrap();
-            },
+            }
+
+            DrawCommand::ScreenPath {
+                color, commands, thickness,
+            } => {
+                draw_path(ctx, *color, commands, *thickness);
+            }
+
+            &DrawCommand::ScreenCircle {
+                thickness, center, radius, color,
+            } => {
+                draw_circle(ctx, thickness, center, radius, color);
+            }
         }
 
-        ctx.restore().unwrap();
+        match &self {
+            DrawCommand::Path { .. } | DrawCommand::Circle { .. } | DrawCommand::Ellipse { .. } => {
+                ctx.restore().unwrap();
+            }
+            _ => {}
+        }
     }
 }
